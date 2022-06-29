423e7237348a64db8322648d7e7aad46b5362a67
Mikhail <mikhailtwere@gmail.com>
2022-06-28 21:20:23 -0600

    commit tree 15e43e9b570614ae0a6f885032e447692e0d8818

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+function destructivelyAppendCat(Chunk){
+    cats.push(Chunk);
+}
+
+function destructivelyPrependCat(Uno){
+    cats.unshift(Uno);
+}
+
+function destructivelyRemoveLastCat(Chunk){
+    cats.pop(Chunk);
+}
+
+function destructivelyRemoveFirstCat(Uno){
+    cats.shift(Uno);
+}
+
+function appendCat(Juno){
+    const catsAgain = cats.slice();
+    catsAgain.push(Juno);
+    return catsAgain;
+}
+
+function prependCat(Pulla){
+    const catsNice = cats.slice("Pulla");
+    catsNice.unshift(Pulla);
+    return catsNice;
+}
+
+function removeLastCat(){
+    const catsRude = cats.slice();
+    catsRude.pop("Garfield");
+    return catsRude;
+}
+
+function removeFirstCat(){
+    const catsRun = cats.slice();
+    catsRun.shift("Milo");
+    return catsRun;
+}
